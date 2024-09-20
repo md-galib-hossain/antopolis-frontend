9d52c616419681876191c4f4a4916f94f68c30ee
Md Galib Hossain <mdgalib23@gmail.com>
2024-09-20 10:38:10 +0600

    NEXT_PUBLIC_BACKEND_LIVE_URL

--- src/hooks/useAxiosSecure.tsx
+++ src/hooks/useAxiosSecure.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosSecure = axios.create({
   // baseURL: `${process.env.NEXT_PUBLIC_BACKEND_LIVE_URL}/api/v1`,
-  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_LOCAL_URL}/api/v1`,
+  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_LIVE_URL}/api/v1`,
 });
 const useAxiosSecure = () => {
   return axiosSecure;
